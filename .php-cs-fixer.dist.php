3e44b0794e76539472f252561ec371acb314c789
Julien M'Poy <julien.mpoy@gmail.com>
2024-04-16 09:32:13 +0200

    Fix PHP-CS-Fixer config and format code

--- .php-cs-fixer.dist.php
+++ .php-cs-fixer.dist.php
@@ -2,9 +2,13 @@
 
 $finder = (new PhpCsFixer\Finder())
     ->in(__DIR__.'/src')
+    ->append([
+        __DIR__.'/.php-cs-fixer.dist.php',
+        __DIR__.'/rector.php',
+    ])
     ->exclude([
         'var',
-        'App\OpenApiBundle'
+        'App\OpenApiBundle',
     ])
 ;
 
